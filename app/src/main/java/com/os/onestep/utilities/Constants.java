d0546e7b17818149bc11a2606ed45a6fe990fa79
MeetBrahmbhatt1 <meetbrahmbhatt91@gmail.com>
2016-10-12 11:52:21 +0530

    New Constants added.

--- app/src/main/java/com/os/onestep/utilities/Constants.java
+++ app/src/main/java/com/os/onestep/utilities/Constants.java
@@ -2,10 +2,21 @@ package com.os.onestep.utilities;
 
 public class Constants {
 
-    public static final String LOGIN_URL = "http://divsinfotech.com/OneStep/api/loginobjvalidate2?";
-    public static final String SIGNUP_URL = "http://divsinfotech.com/OneStep/api/signupact?";
+    public static final String MAIN_URL = "http://divsinfotech.com/OneStep/api/";
+    public static final String LOGIN_URL = MAIN_URL + "loginobjvalidate2?";
+    public static final String SIGNUP_URL = MAIN_URL + "signupact?";
+    public static final String DROPDOWN_URL = MAIN_URL + "fetchdropdown?";
+    public static final String DROP_TYPE_URL = MAIN_URL + "fetchdroptype";
+    public static final String FETCH_EST_URL = MAIN_URL + "fetchintestimates?";
 
     public static final String SUCCESS_KEY = "success";
     public static final String MSG_KEY = "msg";
+    public static final String PROJECT_ID = "project_id";
     public static final String MESSSAGE_KEY = "message";
+    public static final String ID_KEY = "id";
+    public static final String DATA_KEY = "data";
+    public static final String NAME_KEY = "name";
+    public static final String JOB_NAME_KEY = "jobs_name";
+    public static final String SPACE_TYPE_JSON = "Space_Type_Json";
+    public static final String TYPE_JSON = "Type_Json";
 }
